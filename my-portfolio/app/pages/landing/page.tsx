ee2a7dfbaa12a44effa01f25afd2ab88d1b12abc
Hirusha Kularathna <kularathnahirusha@gmail.com>
2025-10-17 23:48:18 +0530

    updated landing page

--- my-portfolio/app/pages/landing/page.tsx
+++ my-portfolio/app/pages/landing/page.tsx
@@ -1,9 +1,10 @@
+import { TypewriterEffectSmoothDemo } from '@/components/TypewriterEffectSmoothDemo'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <TypewriterEffectSmoothDemo />
     </div>
   )
 }
